c6c2f95a7608c1e0533a279f19121ea96c2f403a
teyper <teyjhanabartonneal@spelman.edu>
2024-11-18 11:35:00 -0500

    redo

--- Assets/Tank2/FishLeft.cs
+++ Assets/Tank2/FishLeft.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class FishLeft : MonoBehaviour
 {
-    [SerializeField] float Speed = 2f;
+    [SerializeField] float RotationSpeed = 2f;
+    [SerializeField] float TranslationSpeed;`
     [SerializeField] float Ymin = -5f; // lower boundary 
     [SerializeField] float Ymax = 5f; // upper boundary
     [SerializeField] float Xmin = -5f; // left boundary
@@ -38,52 +39,55 @@ public class FishLeft : MonoBehaviour
 
     void Update()
     {
-        float x = 0f;
-        float y = 0f;
+        //float x = 0f;
+        //float y = 0f;
 
         // control movement using only WASD keys for Player 2
-        if (Input.GetKey(KeyCode.A)) x = -1f;
-        if (Input.GetKey(KeyCode.D)) x = 1f;
-        if (Input.GetKey(KeyCode.W)) y = 1f;
-        if (Input.GetKey(KeyCode.S)) y = -1f;
+        if (Input.GetKey(KeyCode.A)) 
+        {
+            transform.Rotate(0f, 0f, 0.8f);
+            transform.Translate(Vector3.forward * Speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D)) 
+        {
+            transform.Rotate(0f, 0f, -0.8f);
+            transform.Translate(Vector3.forward * Speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.W)) 
+        {
+            transform.Translate(Speed * Time.deltaTime, 0f, 0f);
+            //transform.Translate(Vector3.forward * Speed * Time.deltaTime);
+        }
 
-        transform.Translate(x * Speed * Time.deltaTime, y * Speed * Time.deltaTime, 0f);
-
-        // boundary checks for Player 2
-        if (transform.position.x > Xmax)
-            transform.position = new Vector3(Xmax, transform.position.y, transform.position.z);
-        if (transform.position.x < Xmin)
-            transform.position = new Vector3(Xmin, transform.position.y, transform.position.z);
-        if (transform.position.y > Ymax)
-            transform.position = new Vector3(transform.position.x, Ymax, transform.position.z);
-        if (transform.position.y < Ymin)
-            transform.position = new Vector3(transform.position.x, Ymin, transform.position.z);
+        
 
 
-       
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Debug.Log("hi");
             GameObject obj = Instantiate(ShellPrefab);
             obj.transform.position = transform.position + new Vector3(shell_x_offset, shell_y_offset, 0f);
         }
-        /* void OnTriggerEnter2D(Collider2D collision)
-    {
-        if(collision.gameObject.tag == "shell2")
-            {
-                if (hit == true) return;
 
-                shell = collision.gameObject;
-                hit = true;
-                gameManager.HitByShell2();
-            }
-        if(collision.gameObject.tag== "player2")
-            {
-                animator.SetBool("explode", true);
-                Destroy(gameObject, 1f);
-                audioSource = GetComponent<AudioSource>();
-                audioSource.Play();
-            }
-    }*/
-}
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "shell2")
+        {
+            if (hit == true) return;
+
+            shell = collision.gameObject;
+            hit = true;
+            gameManager.HitByShell2();
+        }
+        if (collision.gameObject.tag == "player2")
+        {
+            animator.SetBool("explode", true);
+            Destroy(gameObject, 1f);
+            //shell = GetComponent<AudioSource>();
+           // audioSource.Play();
+        }
+
+    }
 }
--- Assets/shellLeft/shellmove1.cs
+++ Assets/shellLeft/shellmove1.cs
@@ -6,16 +6,21 @@ public class shellmove : MonoBehaviour
 {
     [SerializeField] float Speed = 4f;
     [SerializeField] float LifeTime = 3f;
+    [SerializeField] Rigidbody2D rigidbody2d;
     // Start is called before the first frame update
     void Start()
     {
         Destroy(gameObject, LifeTime);
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        rigidbody2d.velocity = new Vector3(Speed, 0f, 0f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Speed * Time.deltaTime, 0f, 0f);
+
+        //transform.Translate(Speed * Time.deltaTime, 0f, 0f);
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision) //destroy on collision
